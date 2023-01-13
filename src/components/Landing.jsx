eaff93fff9098b3b9e136e79959ee6c27c4a8d47
kevinchristyy <kevin.10@hotmail.no>
2023-01-13 21:50:30 +0100

    final-push

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,12 +4,12 @@ import BookShelf from "../assets/undraw_book.svg";
 function Landing() {
   return (
     <section id="landing">
-      <header className="header__bgn">
+      <header>
         <div className="header__container">
           <div className="header__description">
             <h1>Norway's most selling online library platform</h1>
             <h2 className="landing__subtitle">
-              Find the book you need with <span className="gold">Library</span>
+              Find the book you need with <span className="gold">ReadBle</span>
             </h2>
             <a href="#features">
               <button className="btn">Find books</button>
