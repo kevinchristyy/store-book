404b869ccbfca997fc05881a2b229cb07c444f8e
kevinchristyy <kevin.10@hotmail.no>
2023-01-13 15:44:26 +0100

    store-done

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BookStack from "../assets/book-stack-library-room.png";
+import BookShelf from "../assets/undraw_book.svg";
 
 function Landing() {
   return (
@@ -7,16 +7,17 @@ function Landing() {
       <header className="header__bgn">
         <div className="header__container">
           <div className="header__description">
-            <h1 className="header__title">
-              Norway's most selling online library platform
-            </h1>
+            <h1>Norway's most selling online library platform</h1>
             <h2 className="landing__subtitle">
-              Find the book you need with <span className="gold">READBLE</span>
+              Find the book you need with <span className="gold">Library</span>
             </h2>
             <a href="#features">
               <button className="btn">Find books</button>
             </a>
           </div>
+          <figure className="header__img--wrapper">
+            <img src={BookShelf} alt="" />
+          </figure>
         </div>
       </header>
     </section>
